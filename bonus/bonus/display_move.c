8c4ee22067a7aee2ea53957d22dbeb6b1a76d6cf
Antoine Collon <acollon@student.s19.be>
2025-06-21 16:59:40 +0200

    bonus so_long anim & move count

---
+++ bonus/bonus/display_move.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display_move.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acollon <acollon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/19 18:31:48 by acollon           #+#    #+#             */
+/*   Updated: 2025/06/19 18:35:34 by acollon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+void	display_move(t_game *game)
+{
+	char	*str;
+
+	str = ft_itoa(game->move_count);
+	if (!str)
+		return ;
+	mlx_string_put(game->mlx, game->window, 10, 10, 0xFFFFFF, "Moves :");
+	mlx_string_put(game->mlx, game->window , 80, 10, 0xFFFFFF, str);
+}
---
+++ mandatory/parsing/error_handler.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_handler.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acollon <acollon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/10 13:19:53 by acollon           #+#    #+#             */
+/*   Updated: 2025/05/23 17:43:38 by acollon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+void	free_all(t_game *game)
+{
+	if (!game)
+		return ;
+	if (game->map)
+		free_map(game->map);
+	if (game->img_coin)
+		mlx_destroy_image(game->mlx, game->img_coin);
+	if (game->img_exit)
+		mlx_destroy_image(game->mlx, game->img_exit);
+	if (game->img_player)
+		mlx_destroy_image(game->mlx, game->img_player);
+	if (game->img_floor)
+		mlx_destroy_image(game->mlx, game->img_floor);
+	if (game->img_wall)
+		mlx_destroy_image(game->mlx, game->img_wall);
+	if (game->window)
+		mlx_destroy_window(game->mlx, game->window);
+	if (game->mlx)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+	}
+}
+
+void	exit_clean(char *msg, t_game *game)
+{
+	free_all(game);
+	if (msg)
+		write(1, msg, ft_strlen(msg));
+	exit(EXIT_SUCCESS);
+}
+
+void	free_and_exit(char *msg, t_game *game)
+{
+	free_all(game);
+	write (2, "Error\n", 6);
+	if (msg)
+		write (2, msg, ft_strlen(msg));
+	exit(EXIT_FAILURE);
+}
+
+int	file_type(t_game *game)
+{
+	int	len;
+
+	if (!game->filename)
+		return (1);
+	len = ft_strlen(game->filename);
+	if (len <= 4)
+		return (1);
+	if (ft_strcmp(game->filename + (len - 4), ".ber") != 0)
+		free_and_exit("Wrong type of file, need .ber\n", game);
+	return (0);
+}
